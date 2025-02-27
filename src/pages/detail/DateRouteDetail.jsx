d43d1033b7b8f4849ee9c58c16e799345e6d7d36
bonseung-dev <glynnregister@gmail.com>
2025-02-27 17:19:22 +0900

    🚚 move: 메인 페이지 로직 상세 페이지로 이동

--- src/pages/detail/DateRouteDetail.jsx
+++ src/pages/detail/DateRouteDetail.jsx
@@ -1,5 +1,15 @@
+// DateRoutePage.jsx
+import { Map } from 'react-kakao-maps-sdk';
+// 작성했던 Kakao 스크립트 로더 컴포넌트 import
+import UseKakaoLoader from '../../components/UseKakaoLoader';
+
 const DateRouteDetail = () => {
-  return <div>DateRouteDetail</div>;
+  return (
+    <div className="w-full h-[600px]">
+      <UseKakaoLoader />
+      <Map id="map" center={{ lat: 33.450701, lng: 126.570667 }} className="w-full h-full" level={3} />
+    </div>
+  );
 };
 
 export default DateRouteDetail;
