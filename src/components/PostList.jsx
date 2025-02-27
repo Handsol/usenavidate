e87d01b062536bce03169e8112df795a9e3cbe32
Handsol <imhandsol@gmail.com>
2025-02-27 19:20:24 +0900

    ✅  add: 게시물 목록 컴포넌트 추가 및 간단 스타일링(그리드 적용)

---
+++ src/components/PostList.jsx
@@ -0,0 +1,23 @@
+const PostList = () => {
+  return (
+    <div className="w-[1200px] grid grid-cols-3 gap-10">
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
+    </div>
+  );
+};
+
+export default PostList;
