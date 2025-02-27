4e3f333e75ba6e949ad79420b96998029cd32462
Handsol <imhandsol@gmail.com>
2025-02-27 13:44:02 +0900

    ✨ feat: Tailwind CSS 색상 팔레트 추가 (톤앤매너 7종 컬러)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        pallette1: '#7469b6',
+        pallette2: '#AD88C6',
+        pallette3: '#E1AFD1',
+        pallette4: '#FFE6E6',
+        pallette5: '#FFFFFF',
+        pallette6: '#343434',
+        pallette7: '#454545',
+        pallette8: '#C05353'
+      }
+    }
   },
   plugins: []
 };
