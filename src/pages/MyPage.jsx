ce993aedd955746e17e3e0635b2cc36243a4cb0f
nahyun park <pnh13584@gmail.com>
2025-02-28 01:31:07 +0900

    💄 style: MyPage 전체 스타일링 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -13,40 +13,38 @@ const MyPage = () => {
     getUserData();
   }, []);
 
-  const createUser = async () => {
-    const { data, error } = await supabase.from('users').insert([
-      {
-        nickname: '허니',
-        email: 'test1@test.com',
-        avatar: 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86072/86072210520_727.jpg'
-      }
-    ]);
-  };
-
   return (
-    <div className=" bg-palette4 text-center text-palette6 items-center w-100 h-100 p-4">
-      <div className="bg-palette1 min-w-xs text-palette5 rounded-md text-2xl">MyList</div>
+    <div className="grid bg-palette4 text-center text-palette6 justify-center w-full h-screen p-4">
+      <div className="grid bg-palette1 w-10% self-center text-palette5 text-2xl rounded-full">MyList</div>
       <div className="py-5">
         <section className="pt-4">
-          <div className="bg-palette2 rounded-md">내가 좋아요한 게시글</div>
-          <div className="pt-4">(여기에 게시글 리스트 뿌리기)</div>
+          <div className="bg-palette2 w-10% text-palette5 text-lg rounded-full">내가 좋아요한 게시글</div>
+          <div className="grid grid-cols-3 gap-4 pt-2">
+            <div className="bg-palette5 w-44 min-h-36 p-1 rounded-2xl" />
+            <div className="bg-palette5 w-44 min-h-36 p-1 rounded-2xl" />
+            <div className="bg-palette5 w-44 min-h-36 p-1 rounded-2xl" />
+          </div>
         </section>
         <section className="pt-4">
-          <div className="bg-palette2 rounded-md">내가 작성한 게시글</div>
-          <div className="pt-4">(여기에 내 게시글 리스트 뿌리기)</div>
+          <div className="bg-palette2 w-10% text-palette5 text-lg rounded-full">내가 작성한 게시글</div>
+          <div className="pt-4">
+            <div /> <div /> <div />
+          </div>
         </section>
       </div>
+
       {/* supabase 테스트용 코드 */}
-
-      {userData.map((item) => (
-        <div key={item.user_id}>
-          <div>{item.email}</div>
-          <div>{item.nickname}</div>
-          <img src={item.avatar} />
-        </div>
-      ))}
-
-      <button onClick={() => createUser()}>데이터 작성 확인용</button>
+      <div className="grid grid-cols-3 gap-4">
+        {userData.map((item) => (
+          <div className="grid bg-palette5 items-center w-10% h-10% p-4 rounded-2xl" key={item.user_id}>
+            <div className="text-lg">{item.email}</div>
+            <div className="text-xs">{item.nickname}</div>
+            <div className="bg-cover overflow-hidden">
+              <img src={item.avatar} className="w-full" alt="..." />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
