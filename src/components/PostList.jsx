a3402953df9b8a41ba6a868daf5c82d32a241fbc
Handsol <imhandsol@gmail.com>
2025-02-27 22:36:58 +0900

    ♻️  refactor: PostList 컴포넌트에서 반복되는 div 제거 및 주석 추가

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,22 +1,6 @@
 const PostList = () => {
   return (
-    <div className="w-[1200px] grid grid-cols-3 gap-10">
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-      <div className="bg-palette1 h-[250px] rounded-3xl"></div>
-    </div>
+    <div className="w-[1200px] grid grid-cols-3 gap-10">{/* 이 사이에는 .map() 으로 필요한 정보만 띄울 예정 */}</div>
   );
 };
 
