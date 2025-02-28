308c52bc23ccb82b6c1c5d0d669d020b114d63a3
nahyun park <pnh13584@gmail.com>
2025-02-28 17:18:04 +0900

    💄 style: ProfilePage 스타일 생성

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,6 +2,31 @@ const ProfilePage = () => {
   return (
     <>
       <div>프로필 페이지</div>
+      <div>
+        <p>왼쪽 구역</p>
+        <div>profile</div>
+        <div>여기에 록기님 인풋창 추가</div>
+      </div>
+      <div>
+        <p>오른쪽 구역</p>
+        <div>
+          <label>닉네임</label>
+          <input placeholder="닉네임" />
+        </div>
+        <div>
+          <label>ID</label>
+          <input placeholder="ID" />
+        </div>
+        <div>
+          <label>password</label>
+          <input placeholder="password" />
+        </div>
+      </div>
+      <div>
+        <p>아래쪽 구역</p>
+        <div>document upload</div>
+        <button>수정하기</button>
+      </div>
     </>
   );
 };
