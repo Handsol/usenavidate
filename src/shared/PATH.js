ddd20e2d3775d6caf784e3ea5ef1cdcc4463e731
nahyun park <pnh13584@gmail.com>
2025-03-01 17:16:07 +0900

    ♻️ refactor: PATH 환경변수 적용

---
+++ src/shared/PATH.js
@@ -0,0 +1,12 @@
+export const PATH = {
+  HOME: '/',
+  LOGIN: '/login',
+  SIGNUP: '/signup',
+  MYPAGE: '/mypage',
+  PROFILE: '/profile',
+  WRITEPOST: '/writepost',
+  DATEDETAIL: '/datedetail',
+  DATEWRITE: '/datewrite',
+  NAVITALK: '/navitalk',
+  NAVITALKDETAIL: '/navitalkdetail'
+};
