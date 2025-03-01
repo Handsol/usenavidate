720c2481d6c44f6ee224fa758bdc4f4f70744aea
nahyun park <pnh13584@gmail.com>
2025-03-01 23:08:37 +0900

    ✅ add: zustand 전체적인 적용 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Router from './shared/Router';
 import useAuthStore from './zustand/AuthStore';
 
 const App = () => {
+  // store에서 값을 꺼내서 사용
   const userData = useAuthStore((state) => state.user);
   const userLogin = useAuthStore((state) => state.userLogin);
   const userLogout = useAuthStore((state) => state.userLogout);
--- src/zustand/AuthStore.js
+++ src/zustand/AuthStore.js
@@ -2,15 +2,20 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const useAuthStore = create(
+  // persist를 사용시 새로고침해도 날아가지 않음
   persist(
     (set) => ({
+      // 인증되어있는지 확인
       isAuthenticated: false,
-      userData: null,
+      //   supabase에서 userData를 받아 사용하는 용도
+      userData: null, // auth table 완성되면 해당 부분 수정 필요
+      // 로그인 state 관리
       userLogin: (user) => set((state) => ({ userData: user, isAuthenticated: true })),
+      // 로그아웃 state 관리
       userLogout: () => set({ userData: null, isAuthenticated: false })
     }),
     {
-      name: 'authenticatedState'
+      name: 'authenticatedState' //localStorage에 저장될 이름
     }
   )
 );
