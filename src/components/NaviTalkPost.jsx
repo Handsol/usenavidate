611fb2ba38a1a748a279d507d0ba48c0efdeab97
Handsol <imhandsol@gmail.com>
2025-03-02 20:22:32 +0900

    ✨ feat: NaviTalkPost 컴포넌트에 클릭 시 게시글 상세 페이지로 이동 기능 추가

--- src/components/NaviTalkPost.jsx
+++ src/components/NaviTalkPost.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
 const NaviTalkPost = ({ post }) => {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate(`/navitalk/${post.posts_id}`);
+  };
+
   return (
-    <div className="relative h-[250px] rounded-3xl overflow-hidden">
+    <div onClick={onClick} className="relative h-[250px] rounded-3xl overflow-hidden">
       <img className="absolute inset-0 w-full h-full object-cover items-center" src={post.posts_img_url} alt="이미지" />
       <section className="absolute bottom-0 w-full h-[100px] flex-col gap-3 bg-palette5">
         <div className="flex flex-row w-full h-[50%] items-center justify-between px-2">
