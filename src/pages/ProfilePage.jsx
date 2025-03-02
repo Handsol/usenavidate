338566ee53948eefb497e4a88c7e4274ae3061c5
nahyun park <pnh13584@gmail.com>
2025-03-02 18:48:34 +0900

    ✅ add: ImageInput 컴포넌트 생성

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,3 +1,5 @@
+import { ImageInput } from '../components/ImageInput';
+
 const ProfilePage = () => {
   return (
     <>
@@ -9,7 +11,7 @@ const ProfilePage = () => {
       <div className="flex flex-row justify-center text-left w-full pt-5 px-40">
         <div className="flex flex-col text-center text-palette6 w-full p-4">
           <p className="text-left text-palette1 text-1xl font-semibold">Profile</p>
-          {/* 여기에 록기님 인풋창 추가 */}
+          <ImageInput />
           <div className="flex flex-shrink-0 bg-palette5 w-10% min-h-52 p-1 rounded-2xl mt-5" />
         </div>
         <div className="flex flex-col text- text-palette6 w-full p-4">
