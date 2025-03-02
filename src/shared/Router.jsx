5305927174b522a0235b406530ae459703b3d556
도니 김 <kdh4847@naver.com>
2025-03-03 00:06:25 +0900

    :sparkles: fullScreen 이 필요한 페이지 관련 처리 로직 추가

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Header from '../components/layout/Header';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
@@ -17,22 +17,30 @@ const Router = () => {
     <BrowserRouter>
       {/* Header 컴포넌트 전역 적용 */}
       <Header />
-      {/* Header 높이만큼 여백 주기 */}
-      <div className="pt-20">
-        <Routes>
-          <Route path={PATH.HOME} element={<DateRoutePage />} />
-          <Route path={PATH.LOGIN} element={<LoginPage />} />
-          <Route path={PATH.SIGNUP} element={<SignupPage />} />
-          <Route path={PATH.MYPAGE} element={<MyPage />} />
-          <Route path={PATH.DATEDETAIL} element={<DateRouteDetail />} />
-          <Route path={PATH.WRITEPOST} element={<WritePostPage />} />
-          <Route path={PATH.NAVITALK} element={<NaviTalkPage />} />
-          <Route path={PATH.NAVITALKDETAIL} element={<NaviTalkDetail />} />
-          <Route path={PATH.DATEWRITE} element={<DateRouteWritePage />} />
-          <Route path={PATH.PROFILE} element={<ProfilePage />} />
-        </Routes>
-      </div>
+      <RouterContents />
     </BrowserRouter>
+  );
+};
+
+const RouterContents = () => {
+  const location = useLocation();
+  const fullScreenPages = [PATH.LOGIN, PATH.SIGNUP, PATH.MYPAGE];
+
+  return (
+    <div className={`${fullScreenPages.includes(location.pathname) ? '' : 'pt-20'}`}>
+      <Routes>
+        <Route path={PATH.HOME} element={<DateRoutePage />} />
+        <Route path={PATH.LOGIN} element={<LoginPage />} />
+        <Route path={PATH.SIGNUP} element={<SignupPage />} />
+        <Route path={PATH.MYPAGE} element={<MyPage />} />
+        <Route path={PATH.DATEDETAIL} element={<DateRouteDetail />} />
+        <Route path={PATH.WRITEPOST} element={<WritePostPage />} />
+        <Route path={PATH.NAVITALK} element={<NaviTalkPage />} />
+        <Route path={PATH.NAVITALKDETAIL} element={<NaviTalkDetail />} />
+        <Route path={PATH.DATEWRITE} element={<DateRouteWritePage />} />
+        <Route path={PATH.PROFILE} element={<ProfilePage />} />
+      </Routes>
+    </div>
   );
 };
 
