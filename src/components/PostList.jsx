54ec596f1b69b4998de445f53fa646fbc8a7a115
Handsol <imhandsol@gmail.com>
2025-03-02 17:32:03 +0900

    ✨ feat: PostList 컴포넌트에 props로 post 정보 내려주기

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,11 @@
 import NaviTalkPost from './NaviTalkPost';
 
-const PostList = () => {
+const PostList = ({ posts }) => {
   return (
     <div className="w-[1300px] grid grid-cols-3 gap-10">
-      {/* Page에서 <PostList> 내부에 넣는 포스트들이 mapping 됩니다 */}
+      {posts.map((post) => (
+        <NaviTalkPost key={post.posts_id} post={post} />
+      ))}
     </div>
   );
 };
