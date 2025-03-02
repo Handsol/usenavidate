c12a1a7530d8d92d49dd150e4453d79e88ab164b
Handsol <imhandsol@gmail.com>
2025-03-02 23:04:35 +0900

    ✨ feat: PostList 컴포넌트에서 boardType에 따라 NaviTalkPost 또는 DateRoutePost 렌더링

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,16 @@
 import NaviTalkPost from './NaviTalkPost';
+import DateRoutePost from './DateRoutePost';
 
-const PostList = ({ posts }) => {
+const PostList = ({ posts, boardType }) => {
   return (
     <div className="w-[1300px] grid grid-cols-3 gap-10">
-      {posts.map((post) => (
-        <NaviTalkPost key={post.posts_id} post={post} />
-      ))}
+      {posts.map((post) =>
+        boardType === 'navitalk' ? (
+          <NaviTalkPost key={post.posts_id} post={post} />
+        ) : (
+          <DateRoutePost key={post.posts_id} post={post} />
+        )
+      )}
     </div>
   );
 };
