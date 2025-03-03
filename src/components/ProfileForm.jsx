ddbc43e242fb3a843201dd46f3d1417292a06fb9
nahyun park <pnh13584@gmail.com>
2025-03-03 18:08:58 +0900

    🩹 fix: name -value 오류 수정

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -8,17 +8,21 @@ const ProfileForm = () => {
     password: ''
   });
 
-  const getUserData = async () => {
-    const { data, error } = await supabase.from('users').select('*');
-  };
+  // const getUserData = async () => {
+  //   const { data, error } = await supabase.from('users').select('*');
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    try {
-      alert('프로필 수정 완료');
-    } catch (error) {
-      alert(error);
-    }
+
+    async (data) => {
+      try {
+        setFormData(data);
+        alert('수정 완료');
+      } catch (error) {
+        alert(error);
+      }
+    };
   };
 
   // 업데이트 로직
@@ -34,44 +38,45 @@ const ProfileForm = () => {
   };
   console.log(formData);
   return (
-    <div className="flex flex-shrink-0">
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col text-palette6 w-full p-4">
-          <div className="flex flex-col gap-3">
-            <label className="text-palette1 text-1xl font-semibold">Nickname</label>
-            <input
-              type="text"
-              className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
-              placeholder="닉네임"
-              value={formData.nickname}
-              onChange={handleChange}
-            />
-            <label className="text-palette1 text-1xl font-semibold">ID</label>
-            <input
-              type="text"
-              className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
-              placeholder="ID"
-              value={formData.id}
-              onChange={handleChange}
-            />
-            <label className="text-palette1 text-1xl font-semibold">password</label>
-            <input
-              type="text"
-              className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
-              placeholder="password"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-          <button
-            className="text-center bg-palette1 text-palette5 items-center w-full rounded-full px-20 py-2 mt-20"
-            type="submit"
-          >
-            수정하기
-          </button>
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col text-palette6 w-full p-4">
+        <div className="flex flex-col gap-3">
+          <label className="text-palette1 text-1xl font-semibold">Nickname</label>
+          <input
+            type="text"
+            name="nickname"
+            className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
+            placeholder="닉네임"
+            value={formData.nickname}
+            onChange={handleChange}
+          />
+          <label className="text-palette1 text-1xl font-semibold">ID</label>
+          <input
+            type="text"
+            name="id"
+            className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
+            placeholder="ID"
+            value={formData.id}
+            onChange={handleChange}
+          />
+          <label className="text-palette1 text-1xl font-semibold">password</label>
+          <input
+            type="text"
+            name="password"
+            className="border-4 border-white rounded-lg bg-inherit p-2 placeholder-palette2"
+            placeholder="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </div>
-      </form>
-    </div>
+        <button
+          className="text-center bg-palette1 text-palette5 items-center w-full rounded-full px-20 py-2 mt-20"
+          type="submit"
+        >
+          수정하기
+        </button>
+      </div>
+    </form>
   );
 };
 
