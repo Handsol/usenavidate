4380cf8dc076aa90965925a73ee5207306aa7edf
Handsol <imhandsol@gmail.com>
2025-03-03 19:59:06 +0900

    ✨ feat: DateRoutePost 및 NaviTalkPost에서 Link 태그를 사용해 라우팅 기능 추가 (디테일 페이지 이동)

--- src/components/DateRoutePost.jsx
+++ src/components/DateRoutePost.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const DateRoutePost = ({ post }) => {
   return (
-    <div className="relative h-[250px] rounded-3xl overflow-hidden cursor-pointer">
+    <Link
+      to={`${PATH.DATEWRITE}/${post.posts_id}`}
+      className="relative h-[250px] rounded-3xl overflow-hidden cursor-pointer"
+    >
       <img className="absolute inset-0 w-full h-full object-cover items-center" src={post.posts_img_url} alt="이미지" />
       <section className="absolute bottom-0 w-full h-[100px] flex-col gap-3 bg-palette5">
         <div className="flex flex-row w-full h-[50%] items-center justify-between px-2">
@@ -20,7 +25,7 @@ const DateRoutePost = ({ post }) => {
           <div className="flex text-lg font-semibold flex-shrink-0 text-palette8">{post.posts_value}만원대</div>
         </div>
       </section>
-    </div>
+    </Link>
   );
 };
 
