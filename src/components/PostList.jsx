474fd61ddcbad0c57baf8ba879ddbbd9dcac9ce8
Handsol <imhandsol@gmail.com>
2025-03-04 14:35:29 +0900

    ✨ Feat: 게시글 작성 버튼 클릭 시 추가 옵션 표시 기능 구현

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,8 +2,11 @@ import NaviTalkPost from './NaviTalkPost';
 import DateRoutePost from './DateRoutePost';
 import { Link } from 'react-router-dom';
 import { PATH } from '../shared/PATH';
+import { useState } from 'react';
 
 const PostList = ({ posts, boardType }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="w-[1300px] grid grid-cols-3 gap-10">
       {posts.map((post) =>
@@ -15,19 +18,24 @@ const PostList = ({ posts, boardType }) => {
       )}
       {/* 게시글 작성 시 클릭할 버튼 */}
       <div className="flex flex-col items-end fixed bottom-6 right-6 gap-3 mr-2 mb-2">
-        <div className="flex flex-row items-center gap-2">
-          <p className="text-right text-2xl font-semibold text-palette2 cursor-default">데이트 루트 작성</p>
-          <Link to={PATH.DATEWRITE}>
-            <img src="/write_post_logo.png" alt="데이트 루트 작성" />
-          </Link>
-        </div>
-        <div className="flex flex-row items-center gap-2">
-          <p className="text-right text-2xl font-semibold text-palette2 cursor-default">장소 리뷰 작성</p>
-          <Link to={PATH.WRITEPOST}>
-            <img src="/write_post_logo.png" alt="네비톡 작성" />
-          </Link>
-        </div>
-        <button>
+        {isOpen && (
+          <>
+            <div className="flex flex-row items-center gap-2">
+              <p className="text-right text-2xl font-semibold text-palette2 cursor-default">데이트 루트 작성</p>
+              <Link to={PATH.DATEWRITE}>
+                <img src="/write_post_logo.png" alt="데이트 루트 작성" />
+              </Link>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <p className="text-right text-2xl font-semibold text-palette2 cursor-default">장소 리뷰 작성</p>
+              <Link to={PATH.WRITEPOST}>
+                <img src="/write_post_logo.png" alt="네비톡 작성" />
+              </Link>
+            </div>
+          </>
+        )}
+
+        <button onClick={() => setIsOpen(!isOpen)}>
           <img src="/write_post_plus.png" alt="게시글 작성" />
         </button>
       </div>
