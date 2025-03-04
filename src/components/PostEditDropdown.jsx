f98b0f70e6933cf0cb3f15682bf0ed7d2341a3bd
Handsol <imhandsol@gmail.com>
2025-03-05 04:10:16 +0900

    ♻️ refactor : PostEditDropdown 컴포넌트 수정 중(import 변경)

--- src/components/PostEditDropdown.jsx
+++ src/components/PostEditDropdown.jsx
@@ -1,7 +1,7 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { PencilIcon, TrashIcon } from '@heroicons/react/16/solid';
-import { useAuthUser } from '../supabase/SupabaseAuth';
 import { useNavigate } from 'react-router-dom';
+import { useAuthUser } from '../hooks/useAuthUser';
 
 const PostEditDropDown = ({ post, updatePost, deletePost }) => {
   const { user, loading } = useAuthUser();
