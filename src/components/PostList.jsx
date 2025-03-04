4f4ec520fc94dd5867c4faf7c32b959a118135fc
Handsol <imhandsol@gmail.com>
2025-03-04 14:30:13 +0900

    ✨ Feat: 게시글 작성 버튼을 링크로 변경하여 내비게이션 추가

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,7 @@
 import NaviTalkPost from './NaviTalkPost';
 import DateRoutePost from './DateRoutePost';
+import { Link } from 'react-router-dom';
+import { PATH } from '../shared/PATH';
 
 const PostList = ({ posts, boardType }) => {
   return (
@@ -15,15 +17,15 @@ const PostList = ({ posts, boardType }) => {
       <div className="flex flex-col items-end fixed bottom-6 right-6 gap-3 mr-2 mb-2">
         <div className="flex flex-row items-center gap-2">
           <p className="text-right text-2xl font-semibold text-palette2 cursor-default">데이트 루트 작성</p>
-          <button>
+          <Link to={PATH.DATEWRITE}>
             <img src="/write_post_logo.png" alt="데이트 루트 작성" />
-          </button>
+          </Link>
         </div>
         <div className="flex flex-row items-center gap-2">
           <p className="text-right text-2xl font-semibold text-palette2 cursor-default">장소 리뷰 작성</p>
-          <button>
+          <Link to={PATH.WRITEPOST}>
             <img src="/write_post_logo.png" alt="네비톡 작성" />
-          </button>
+          </Link>
         </div>
         <button>
           <img src="/write_post_plus.png" alt="게시글 작성" />
