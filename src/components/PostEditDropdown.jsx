d2f820f7ad8cc820df1a9c103fc30ae10fd5869b
Handsol <imhandsol@gmail.com>
2025-03-04 18:25:47 +0900

    ✨ Feat: 로그인한 사용자의 게시글일 경우에만 버튼이 나타나도록 구현

--- src/components/PostEditDropdown.jsx
+++ src/components/PostEditDropdown.jsx
@@ -1,33 +1,38 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { PencilIcon, TrashIcon } from '@heroicons/react/16/solid';
+import { useAuthUser } from '../supabase/SupabaseAuth';
 
-export default function Example() {
+const PostEditDropDown = ({ post }) => {
+  const { user, loading } = useAuthUser();
+
+  if (loading) return null;
+
+  console.log('🔍 로그인한 사용자 ID:', user?.id);
+  console.log('🔍 게시글 작성자 ID:', post?.users_id);
+
   return (
-    <div>
+    user?.id === post.users_id && (
       <Menu>
         <MenuButton>
-          <img src="/post-edit.png" alt="Edit" className="w-[50px] h-[50px]" />
+          <img src="/post-edit.png" alt="게시글 옵션" className="w-8 h-8" />
         </MenuButton>
-
-        <MenuItems
-          transition
-          anchor="bottom end"
-          className="w-64 origin-top-right rounded-xl border border-white/5 bg-white p-2 text-lg text-palette3 transition-all duration-200 ease-out transform translate-y-5 opacity-0 data-[open]:translate-y-0 data-[open]:opacity-100 [--anchor-gap:20px] focus:outline-none"
-        >
+        <MenuItems className="w-52 bg-white text-palette3 shadow-md rounded-lg p-2">
           <MenuItem>
-            <button className="group flex w-full items-center gap-3 rounded-lg py-2 px-4 hover:bg-gray-100">
-              <PencilIcon className="size-6 fill-palette3" />
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100">
+              <PencilIcon className="w-5 h-5 text-palette3" />
               게시글 수정
             </button>
           </MenuItem>
           <MenuItem>
-            <button className="group flex w-full items-center gap-3 rounded-lg py-2 px-4 hover:bg-gray-100">
-              <TrashIcon className="size-6 fill-palette3" />
+            <button className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-100">
+              <TrashIcon className="w-5 h-5 text-palette3" />
               게시글 삭제
             </button>
           </MenuItem>
         </MenuItems>
       </Menu>
-    </div>
+    )
   );
-}
+};
+
+export default PostEditDropDown;
