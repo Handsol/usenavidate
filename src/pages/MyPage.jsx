5981925bcbb61312a825ae274d42a028d91fd145
nahyun park <pnh13584@gmail.com>
2025-03-04 10:23:03 +0900

    🔥 del: 사용하지 않는 포스트 리스트 삭제

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -18,22 +18,11 @@ const MyPage = () => {
       <div className="flex bg-palette1 w-10% self-center text-palette5 text-2xl rounded-full px-8 py-2">MyList</div>
       <section className="flex flex-col flex-grow-0 justify-center pt-4">
         <div className="bg-palette2 w-10% self-center text-palette5 text-lg rounded-full px-8 py-2 mt-4">
-          내가 좋아요한 게시글
-          {/* 데이트 코스 포스트(피드) 리스트 레이아웃 그대로 가져올 예정 아래는 스켈레톤 예시 */}
-        </div>
-        <div className="flex flex-shrink-0 justify-center gap-12 pt-2 mt-2">
-          <div className="bg-palette5 w-96 min-h-52 p-1 rounded-2xl" />
-          <div className="bg-palette5 w-96 min-h-52 p-1 rounded-2xl" />
-          <div className="bg-palette5 w-96 min-h-52 p-1 rounded-2xl" />
-        </div>
-      </section>
-      <section className="flex flex-col flex-grow-0 justify-center pt-4">
-        <div className="bg-palette2 w-10% self-center text-palette5 text-lg rounded-full px-8 py-2 mt-4">
           내가 작성한 게시글
         </div>{' '}
         {/* 데이트 코스 포스트(피드) 리스트 레이아웃 그대로 가져올 예정 */}
         {/* supabase 확인용으로 user 가입 정보로 작성했으니 예시로만 봐주세요  */}
-        <div className="flex flex-shrink-0 justify-center gap-12 pt-2 mt-2">
+        <div className="flex flex-shrink-0 flex-wrap justify-center gap-12 pt-2 mt-2">
           {userData.map((item) => (
             <div className="bg-palette5 self-center w-10% h-10% p-2 rounded-2xl" key={item.user_id}>
               <div className="text-lg">{item.users_email}</div>
