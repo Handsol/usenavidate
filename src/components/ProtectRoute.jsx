1600b3c9eccb3538f5fc9d64c2dc61fa1ede7b0a
도니 김 <kdh4847@naver.com>
2025-03-04 22:15:31 +0900

    :sparkles: 로그인 여부에 따른 전역 상태 관리

--- src/components/ProtectRoute.jsx
+++ src/components/ProtectRoute.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { PATH } from '../shared/PATH';
 import { useEffect } from 'react';
 import { AlertError } from '../common/Alert';
