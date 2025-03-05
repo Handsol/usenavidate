5f3c276d753396d411effa02c01201a485fcfa35
도니 김 <kdh4847@naver.com>
2025-03-05 09:28:34 +0900

    :lipstick: UI 변경

--- src/components/DateRoutePost.jsx
+++ src/components/DateRoutePost.jsx
@@ -10,7 +10,7 @@ const DateRoutePost = ({ post }) => {
       <img className="absolute inset-0 w-full h-full object-cover items-center" src={post.posts_img_url} alt="이미지" />
       <section className="absolute bottom-0 w-full h-24 flex-col bg-palette5">
         <div className="flex flex-row w-full h-1/2 items-center justify-between px-4 pt-2">
-          <div className="flex w-full text-xl text-palette7 font-semibold">{post.posts_title}</div>
+          <div className="flex w-full text-xl text-palette7 font-semibold truncate ">{post.posts_title}</div>
           <div className="flex text-md font-semibold flex-shrink-0 text-palette7">⭐{post.posts_review}</div>
         </div>
         <div className="flex flex-row w-full h-1/2 items-center justify-between px-4 pb-3">
